fbb98b951ad352ada302fff6ed2270b1d5390414
tauhid495 <tauhid495@gmail.com>
2022-02-21 17:59:53 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,44 @@
+// let Tauhid = { mobile: 12000, honda: 10000, bari: 2000000 };
+
+// let text = `Welcome Mr ${Tauhid.honda} ${Tauhid.bari}`;
+// console.log(text);
+
+3.1
+// let gun = a => {
+//     let result = a / 5;
+//     return result;
+// }
+// let result2 = gun(1);
+// console.log(result2);
+// // 3.2 3.4
+// let sum = (a, b) => {
+
+//     let a1 = (a + 2);
+//     let b1 = (b + 2);
+//     let result = a1 * b1;
+//     return result;
+// }
+
+// let result1 = sum(1, 2);
+// console.log(result1);
+
+3.3
+// let goon = (a, b, c) => {
+//     let result = a * b * c;
+//     return result;
+// }
+
+// let result1 = goon(2, 2, 2);
+// console.log(result1);
+
+// 5
+// const map1 = [2, 3, 4, 5].map(x => x * 5);
+// console.log(map1);
+
+// 6
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const odd = array.filter(number => {
+//     return (number % 2 != 0);
+// })
+// console.log(odd);
+
