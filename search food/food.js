d4cde7819722d8c063d01e2f1f5b27b04f50cc93
tauhid495 <tauhid495@gmail.com>
2022-02-23 16:42:46 +0600

    hh

--- search food/food.js
+++ search food/food.js
@@ -9,5 +9,24 @@ const searchFood = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.meals));
+        .then(data => displayResult(data.meals));
+}
+
+const displayResult = meals => {
+    const searchResult = document.getElementById('searchResult');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
